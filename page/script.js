f202ccfc5ec44706ac3d1cdb2271c795e271aaca
cokeliko <skuskudusku@outlook.fr>
2024-01-26 18:39:07 +0100

    git commit - Ajout de texte, d'images et de styles CSS. Refonte de la structure HTML, correction de bugs dans le bouton de la sidebar. Mise en place d'effets visuels au survol des liens. Ajustement des styles pour une meilleure lisibilité. Modification du comportement du bouton pour qu'il suive la sidebar. Réorganisation et nettoyage du code.

--- page/script.js
+++ page/script.js
@@ -2,3 +2,8 @@ const sidebar = document.getElementById('sidebar');
 const toggleBtn = document.getElementById('toggle-btn');
 
 toggleBtn.addEventListener('click', () => sidebar.classList.toggle('visible'));
+
+document.getElementById('toggle-btn').addEventListener('click', function () {
+  document.getElementById('sidebar').classList.toggle('sidebar-open');
+  toggleBtn.classList.toggle('animate');
+});
