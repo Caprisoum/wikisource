5fe07dd5b1fc6e8cc122125c0fa05b367a8bcec1
cokeliko <skuskudusku@outlook.fr>
2024-01-29 18:08:33 +0100

    chore: minor changes

--- page/script.js
+++ page/script.js
@@ -3,7 +3,7 @@ const toggleBtn = document.getElementById('toggle-btn');
 
 toggleBtn.addEventListener('click', () => sidebar.classList.toggle('visible'));
 
-document.getElementById('toggle-btn').addEventListener('click', function () {
+document.getElementById('toggle-btn').addEventListener('click', () => {
   document.getElementById('sidebar').classList.toggle('sidebar-open');
   toggleBtn.classList.toggle('animate');
 });
