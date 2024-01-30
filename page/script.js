9f6a3a7fbfcffe6dde7d97d478540add7e161a3e
cokeliko <skuskudusku@outlook.fr>
2024-01-30 16:09:57 +0100

    git add Ajout de la feuille de style pour le site Antiquité Ajout d'un bouton remonte en haut de la page

--- page/script.js
+++ page/script.js
@@ -7,3 +7,20 @@ document.getElementById('toggle-btn').addEventListener('click', () => {
   document.getElementById('sidebar').classList.toggle('sidebar-open');
   toggleBtn.classList.toggle('animate');
 });
+
+const goToTopBtn = document.getElementById('go-to-top-btn');
+
+goToTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 500) {
+    goToTopBtn.classList.add('visible');
+  } else {
+    goToTopBtn.classList.remove('visible');
+  }
+});
